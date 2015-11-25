2544ef4856a71ecb67839404fd1f2b6fc0496056
Richard Gautereaux <ReliX@Mothership-Zeta.local>
2015-11-24 22:35:11 -0800

    Heroku launch

--- routes/links.js
+++ routes/links.js
@@ -4,6 +4,7 @@
 var express = require('express');
 var router = express.Router();
 var Link = require('../models/link')
+var Tab = require('../models/tab')
 
 router.get('/', function(req,res){
   res.send('We Got It!')
@@ -14,6 +15,23 @@ router.post('/create', function(req, res){
    console.log('link created: ', link);
    res.status((err) ? 400 : 200).send( err || link);
   })
+})
+
+router.delete('/', function(req, res){
+  Link.findOne({linkUrl: req.body.linkUrl}, function(err, link){
+    if(err) return res.send(err, 400);
+    Tab.find({ links: link._id }, function(err,tabs){
+      if(err) return res.send(err, 400);
+      tabs.forEach(function(tab){
+        tab.links.splice(tab.links.indexOf(link._id),1);
+        tab.save(function(err){
+          if(err) return res.send('Error saving tags', 400) ;
+        })
+      })
+      res.send('Sucessfully Deleted link', 200)
+    })
+  })
+  //Link.remove({linkUrl: req.body.linkUrl}, function)
 })
 
 module.exports = router
--- routes/tabs.js
+++ routes/tabs.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var router = express.Router();
 var Tab = require('../models/tab')
+var Link = require('../models/link')
 
 router.get('/', function(req,res){
   Tab.find({}, function(err, tabs){
@@ -10,21 +11,27 @@ router.get('/', function(req,res){
   });
 });
 
-//router
+router.post('/addLink', function(req,res){
+  Link.findOne({linkUrl: req.body.linkUrl}, function(err, foundLink){
+    if(err){return res.send(err, 400)}
+    Tab.findOneAndUpdate({tabName : req.body.tabName}, {$push: {links: foundLink._id }}, function(err, foundTab){
+    res.status((err) ? 400 : 200).send( err || foundTab);
+    });
+  });
+});
 
 router.delete('/', function(req, res){
   var tabToRemove = req.body
-  Tab.findOne({tabName: tabToRemove.tabName }, function(err, foundTab){
-    console.log("Found Tbabababa", foundTab)
-    if(err) {return res.send(err, 400)}
-    if(foundTab.links.length){
-      return res.send('You Cannot Delete A Tab With Links Attached!')
-    }
-  Tab.remove({tabName: tabToRemove.tabName }, function(err, removedTab){
-    console.log('tag Removed:  ', removedTab);
-    res.status((err) ? 400 : 200).send( err || removedTab );
-  })
-  });
+    Tab.findOne({tabName: tabToRemove.tabName }, function(err, foundTab){
+      if(err) {return res.send(err, 400)}
+      if(foundTab.links.length){
+        return res.send('You Cannot Delete A Tab With Links Attached!')
+      }
+      Tab.remove({tabName: tabToRemove.tabName }, function(err, removedTab){
+        console.log('tag Removed:  ', removedTab);
+        res.status((err) ? 400 : 200).send( err || removedTab );
+      })
+    });
 })
 
 router.post('/create', function(req, res){
