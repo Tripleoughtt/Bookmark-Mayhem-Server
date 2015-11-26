7cb75fb4fda62ae1aee57262d51d62c1a7b1d78a
Richard Gautereaux <ReliX@Mothership-Zeta.local>
2015-11-25 21:59:17 -0800

    Added more error catching

--- app.js
+++ app.js
@@ -32,10 +32,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
   next();
 });
 
